048f6ad87efdef58eb3d68b9e27ae35a67375a69
p114514 <peter20030725@gmail.com>
2023-03-26 20:40:35 +0800

    1.2
    Player fully animated

--- DSproject/support.py
+++ DSproject/support.py
@@ -8,7 +8,7 @@ def import_folder(path):
 
     for folder_name, sub_folder, img_files in walk(path):
         for img in img_files:
-            full_path = path + '/' + img
+            full_path = path + '\\' + img
             img_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(img_surf)
 
