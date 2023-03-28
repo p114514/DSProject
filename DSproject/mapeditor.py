29ed0f1052c63838fb9bdae22eb2615b6d7adce1
明 <2652581159@qq.com>
2023-03-28 20:32:01 +0800

    修改player 增加地图边缘检测 但未增加迷宫边缘检测

--- DSproject/mapeditor.py
+++ DSproject/mapeditor.py
@@ -11,23 +11,39 @@ class myMap():
         #modify maze
         self.mazerow=4;
         self.mazecol=4;
-        self.mazelist=np.array([[0,1,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,1]])
-
+        self.mazelist=np.array([[0,1,1,0],
+                                [1,1,0,0],
+                                [1,1,0,0],
+                                [1,1,1,1]])
+        self.MoveArea = [[1 for i in range(0,SCREEN_WIDTH)]for j in range(0,SCREEN_HEIGHT)]
         #define para
         self.screen=screen
-        self.xl=SCREEN_WIDTH// self.mazerow
-        self.yl=SCREEN_HEIGHT//self.mazecol
-        self.cell_row=range(0,SCREEN_WIDTH,self.xl)
-        self.cell_col = range(0, SCREEN_HEIGHT,self.yl)
-
-
-
+        self.xl=SCREEN_WIDTH// self.mazecol
+        self.yl=SCREEN_HEIGHT//self.mazerow
+        self.cell_row=range(0,SCREEN_HEIGHT,self.yl)
+        self.cell_col = range(0, SCREEN_WIDTH,self.xl)
     def drawWall(self):
         for i in range(0,self.mazerow):
             for j in range(0,self.mazecol):
                 if self.mazelist[i][j]==0:
-                 pygame.draw.rect(self.screen,(255,255,255),(self.cell_row[i],self.cell_col[j],self.xl,self.yl))
+                  pygame.draw.rect(self.screen,(255,255,255),(self.cell_col[j],self.cell_row[i],self.xl,self.yl))
+                  for k in range(i*self.yl, self.xl):
+                      for p in range(j*self.xl , self.yl):
+                         self.MoveArea[k][p]=0
 
+
+
+
+
+    def getMoveArea(self):
+        return self.MoveArea
+    def getMovePos(self):
+        movepos=[]
+        for i in range(0,SCREEN_HEIGHT):
+            for j in range(0,SCREEN_WIDTH):
+                if self.MoveArea[i][j]==1:
+                    movepos.append((i,j))
+        return movepos
     def getmaze(self):
         return self.mazelist
     def getmazerow(self):
